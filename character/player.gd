ed3f39a7c8c8b19e4707f3af63af0239bfbf7491
maciulex <41756057+maciulex@users.noreply.github.com>
2023-03-17 18:59:57 +0100

    animation, tool Axe
    Added idle state for every direction
    added toolCoolDown

--- character/player.gd
+++ character/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @export var movement_speed : float = 100;
 @export var knotbackStrength: float = 400;
+@export var toolCoolDownTime : float = 1.5;
 @onready var animation_tree = $AnimationTree;
 var KnockbackRays : Array;
 signal building;
@@ -12,13 +13,13 @@ func _ready():
 	playerData = get_node("/root/PlayerData");
 	self.health.connect($Control/HPValLabel.updateHp);
 	KnockbackRays = [
-		$RayCastsContainer/RayCast2D1,#bottom-middle
+		$RayCastsContainer/RayCast2D1,#bottom-middle					
 		$RayCastsContainer/RayCast2D2,#next going clockwise by 45 deg
-		$RayCastsContainer/RayCast2D3,
+		$RayCastsContainer/RayCast2D3,#left
 		$RayCastsContainer/RayCast2D4,
-		$RayCastsContainer/RayCast2D5,
+		$RayCastsContainer/RayCast2D5,#top
 		$RayCastsContainer/RayCast2D6,
-		$RayCastsContainer/RayCast2D7,
+		$RayCastsContainer/RayCast2D7,#right
 		$RayCastsContainer/RayCast2D8
 	];
 	
@@ -27,11 +28,15 @@ func _physics_process(delta):
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
+	if (playerData.movementBlock):
+		return;	
+	
 	var toolUse = Input.get_action_strength("space");
-	if (toolUse):
+	if (toolUse && !playerData.toolCoolDown):
+		$Timer.start(toolCoolDownTime);
+		playerData.toolCoolDown = true;
 		useTool(input_direction);
-		
-	if (!playerData.movementBlock):
+	else:
 		velocity = input_direction * movement_speed;
 		update_animation("movement",input_direction)
 		move_and_slide();
@@ -41,7 +46,10 @@ func animationForWalkOrIdle(move_input : Vector2):
 	if (move_input != Vector2.ZERO):
 		animation_tree.set("parameters/conditions/IsMoving", true);
 		animation_tree.set("parameters/conditions/isIdle", false);
-		animation_tree.set("parameters/walk/blend_position", move_input);
+		playerData.lookingDirection = move_input;
+		animation_tree.set("parameters/walk/blend_position", playerData.lookingDirection);
+		animation_tree.set("parameters/idle/blend_position", playerData.lookingDirection);
+		
 	else:
 		animation_tree.set("parameters/conditions/IsMoving", false);		
 		animation_tree.set("parameters/conditions/isIdle", true);
@@ -50,7 +58,7 @@ func animationForTools(vector : Vector2):
 	animation_tree.set("parameters/conditions/IsMoving", false);
 	animation_tree.set("parameters/conditions/isIdle", false);
 	animation_tree.set("parameters/conditions/useTool", true);
-	animation_tree.set("parameters/UseTool/UseAxe/blend_position", vector);	
+	animation_tree.set("parameters/UseTool/UseAxe/blend_position", playerData.lookingDirection);	
 	
 	pass;
 
@@ -85,9 +93,12 @@ func blockPlayerMovement():
 	update_animation("movement", Vector2.ZERO);
 
 func useTool(lookingAt: Vector2):
+	if (lookingAt == Vector2.ZERO): lookingAt = Vector2(0,1);
 	blockPlayerMovement();
 	update_animation("toolUse", lookingAt);
-	
+	match playerData.equipedTool:
+		playerData.tools.Axe:
+			pass;
 	pass;
 
 	
@@ -116,8 +127,12 @@ func _on_character_collision_body_entered(body):
 func _on_animation_tree_animation_finished(anim_name):
 	print("1 ",anim_name);
 	if (anim_name.contains("Use")):
+		animation_tree.set("parameters/conditions/isIdle", true);
 		animation_tree.set("parameters/conditions/useTool", false);
-		
 		playerData.movementBlock = false;
 
-
+#function act as guard to ensure that 
+func _on_timer_timeout():
+	if (playerData.toolCoolDown):
+		playerData.toolCoolDown = false;
+	pass # Replace with function body.
--- player_data.gd
+++ player_data.gd
@@ -1,6 +1,10 @@
 extends Node
 
+enum tools {Axe};
 
 @export var health : int = 100;
 @export var movementBlock : bool = false;
+@export var toolCoolDown : bool =  false;
+var lookingDirection : Vector2 = Vector2.ZERO;
+var equipedTool = tools.Axe;
 var cordsBeforeEntringBuilding = Vector2(0,0);
